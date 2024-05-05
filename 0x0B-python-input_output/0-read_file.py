17a497a9515b3f0c534dfc686c3ac3979065de5a
HarakaSana11 <mwangipkpipi@gmail.com>
2024-05-04 23:48:32 -0700

    Reads a text file and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
 Module 0-read_file
-contains function hat reads and prints from file
+
+Contains function that reads and prints contents from file
 """
 
+
 def read_file(filename=""):
-    """Reads and prints text from file"""
-    with open(filename, mode="r", encoding="utf=8") as f:
-       print(f.read(), end"")
+    """Read and print text from file"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read(), end="")
